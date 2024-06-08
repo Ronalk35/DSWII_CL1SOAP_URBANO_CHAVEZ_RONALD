974c649abbc5ea8bd84faee02ad563893157f20e
Ronalk <sam.ronaldurbano@gmail.com>
2024-06-08 11:46:24 -0500

    Creacion DSWII_CL1SOAP_URBANO_CHAVEZ_RONALD con la importación de las dependencias necesarias para un proyecto SOAP, y la estructura aprendida

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_URBANO_CHAVEZ_RONALD/DswiiCl1SoapUrbanoChavezRonaldApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_URBANO_CHAVEZ_RONALD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapUrbanoChavezRonaldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapUrbanoChavezRonaldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL1SOAP_URBANO_CHAVEZ_RONALD/DswiiCl1SoapUrbanoChavezRonaldApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_URBANO_CHAVEZ_RONALD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapUrbanoChavezRonaldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
